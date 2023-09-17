53448fe732951b97d4a7be3695a3194766188206
Fernando Humberto <113042135+fernandohumberto@users.noreply.github.com>
2023-09-17 19:32:24 -0300

    Correção de errros

--- Tela_Inicial/Javascript/script.js
+++ Tela_Inicial/Javascript/script.js
@@ -10,10 +10,6 @@ allSideMenu.forEach(item=> {
 		li.classList.add('active');
 	})
 });
-
-
-
-
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
 const sidebar = document.getElementById('sidebar');
@@ -21,13 +17,6 @@ const sidebar = document.getElementById('sidebar');
 menuBar.addEventListener('click', function () {
 	sidebar.classList.toggle('hide');
 })
-
-
-
-
-
-
-
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
