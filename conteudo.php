5a27471699e09e60b2cd7991379ff9d584341b8d
Marco Davi <marcodavibdemelo@gmail.com>
2023-09-21 17:15:14 -0300

    //

--- conteudo.php
+++ conteudo.php
@@ -153,7 +153,7 @@
                             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                     <div class="avatar avatar-online">
-                                        <img src="img/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                        <img src="img/fotodeperfilbase.svg" alt class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end">
@@ -162,7 +162,7 @@
                                             <div class="d-flex">
                                                 <div class="flex-shrink-0 me-3">
                                                     <div class="avatar avatar-online">
-                                                        <img src="img/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                                        <img src="img/fotodeperfilbase.svg" alt class="w-px-40 h-auto rounded-circle" />
                                                     </div>
                                                 </div>
                                                 <div class="flex-grow-1">
