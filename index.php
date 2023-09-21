4dfb5b13e301abffdd37faa6d06fd39b07cad3e9
Marco Davi <marcodavibdemelo@gmail.com>
2023-09-21 17:05:00 -0300

    //

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
                             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                     <div class="avatar avatar-online">
-                                        <img src="img/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                        <img src="img/fotodeperfilbase.svg" alt class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end">
@@ -157,7 +157,7 @@
                                             <div class="d-flex">
                                                 <div class="flex-shrink-0 me-3">
                                                     <div class="avatar avatar-online">
-                                                        <img src="img/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                                        <img src="img/fotodeperfilbase.svg" alt class="w-px-40 h-auto rounded-circle" />
                                                     </div>
                                                 </div>
                                                 <div class="flex-grow-1">
