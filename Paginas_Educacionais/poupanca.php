bfab93c879552fda757676ffed11a9a641cea4b8
Marco Davi <marcodavibdemelo@gmail.com>
2023-10-22 12:16:40 -0300

    //

--- Paginas_Educacionais/poupanca.php
+++ Paginas_Educacionais/poupanca.php
@@ -49,7 +49,7 @@
                         <span class="app-brand-text demo menu-text fw-bolder ms-2">Imperium <br>Vastare </span>
                     </a>
 
-                    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none" onclick="removeClasse()">
                         <i class="bx bx-chevron-left bx-sm align-middle"></i>
                     </a>
                 </div>
--- faq.php
+++ faq.php
@@ -17,7 +17,7 @@ include_once ('config.php') ;
                         <span class="app-brand-text demo menu-text fw-bolder ms-2">Imperium <br>Vastare </span>
                     </a>
 
-                    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none" onclick="removeClasse()">
                         <i class="bx bx-chevron-left bx-sm align-middle"></i>
                     </a>
                 </div>
